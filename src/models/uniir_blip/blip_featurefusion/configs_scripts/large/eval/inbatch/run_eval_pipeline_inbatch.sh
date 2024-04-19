63154069f1c8101691962d51f63e5942e8d6bd06
Naveen Reddy Dyava <nd2794@bader.cm.cluster>
2024-04-18 22:08:36 -0400

    dkajbdkabdab

--- src/models/uniir_blip/blip_featurefusion/configs_scripts/large/eval/inbatch/run_eval_pipeline_inbatch.sh
+++ src/models/uniir_blip/blip_featurefusion/configs_scripts/large/eval/inbatch/run_eval_pipeline_inbatch.sh
@@ -3,17 +3,17 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 # Initialize Conda
-source /home/miniconda3/etc/profile.d/conda.sh # <--- Change this to the path of your conda.sh
+source /burg/opt/anaconda3-2022.05/etc/profile.d/conda.sh # <--- Change this to the path of your conda.sh
 
 # Path to the codebase and config file
-SRC="$HOME/UniIR/src"  # Absolute path to codebse /UniIR/src # <--- Change this to the path of your UniIR/src
+SRC="/burg/dsi/users/nd2794/mmir/UniIR/src"  # Absolute path to codebse /UniIR/src # <--- Change this to the path of your UniIR/src
 
 # Path to common dir
 COMMON_DIR="$SRC/common"
 
 # Path to MBEIR data and UniIR directory where we store the checkpoints, embeddings, etc.
-UNIIR_DIR="/data/UniIR/" # <--- Change this to the UniIR directory
-MBEIR_DATA_DIR="/data/UniIR/M-BEIR/" # <--- Change this to the MBEIR data directory you download from HF page
+UNIIR_DIR="/burg/dsi/users/nd2794/mmir" # <--- Change this to the UniIR directory
+MBEIR_DATA_DIR="/burg/dsi/users/nd2794/mmir/M-BEIR" # <--- Change this to the MBEIR data directory you download from HF page
 
 # Path to config dir
 MODEL="uniir_blip/blip_featurefusion"  # <--- Change this to the model you want to run
@@ -24,8 +24,8 @@ EXP_NAME="inbatch"
 CONFIG_DIR="$MODEL_DIR/configs_scripts/$SIZE/$MODE/$EXP_NAME"
 
 # Set CUDA devices and PYTHONPATH
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7  # <--- Change this to the CUDA devices you want to use
-NPROC=8 # <--- Change this to the number of GPUs you want to use
+export CUDA_VISIBLE_DEVICES=0,1  # <--- Change this to the CUDA devices you want to use
+NPROC=2 # <--- Change this to the number of GPUs you want to use
 export PYTHONPATH=$SRC
 echo "PYTHONPATH: $PYTHONPATH"
 echo  "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
@@ -35,7 +35,7 @@ cd $COMMON_DIR
 
 # Activate conda environment
 # conda activate blip
-conda activate uniir # <--- Change this to the name of your conda environment
+conda activate /burg/dsi/users/nd2794/mmir/envs/uniir # <--- Change this to the name of your conda environment
 
 # Run Embedding command
 CONFIG_PATH="$CONFIG_DIR/embed.yaml"
@@ -54,7 +54,7 @@ python -m torch.distributed.run --nproc_per_node=$NPROC $SCRIPT_NAME \
     --mbeir_data_dir "$MBEIR_DATA_DIR"
 
 # Activate faiss environment
-conda activate faiss # <--- Change this to the name of your conda environment
+conda activate /burg/dsi/users/nd2794/mmir/envs/faiss # <--- Change this to the name of your conda environment
 
 # Run Index command
 CONFIG_PATH="$CONFIG_DIR/index.yaml"
